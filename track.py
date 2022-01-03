fd6a90405b4f4a554f242c3c5ae86143919974ef
DevEvil <73029696+DevEvil99@users.noreply.github.com>
2022-01-03 20:40:56 +0330

    Update track.py

--- track.py
+++ track.py
@@ -1,3 +1,4 @@
+from phonenumbers import carrier
 import phonenumbers
 from pn import number
 
@@ -6,7 +7,7 @@ ch_number = phonenumbers.parse(number, "CH")
 print(geocoder.description_for_number(ch_number, "en"))
 
 
-from phonenumbers import carrier
 service_number = phonenumbers.parse(number, "RO")
 print(carrier.name_for_number(service_number, "en"))
 
+# Copyright 2022 DevEvil.xyz
